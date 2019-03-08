aa74996a86b7a864fac77c0ff64a799cdf7cb1fa
Maximilien Tantin <maximilien.tantin@horanet.com>
2019-03-08 14:04:23 +0100

    Change amount calculation to avoid rounding issues

--- odoo/addons/payment_systempay/models/inherited_payment_acquirer.py
+++ odoo/addons/payment_systempay/models/inherited_payment_acquirer.py
@@ -6,6 +6,7 @@ import uuid
 from datetime import datetime
 
 from odoo import api, fields, models
+from odoo.tools import float_round
 
 
 class SystemPayAcquirer(models.Model):
@@ -76,6 +77,7 @@ class SystemPayAcquirer(models.Model):
         self.ensure_one()
 
         base_url = self.env['ir.config_parameter'].get_param('web.base.url')
+        prec = self.env['decimal.precision'].precision_get('Product Price')
 
         mode = 'TEST'
         if self.environment == 'prod':
@@ -85,7 +87,7 @@ class SystemPayAcquirer(models.Model):
         systempay_tx_values = dict(values)
         systempay_tx_values.update({
             'vads_site_id': self.systempay_shop_id,
-            'vads_amount': int(values['amount'] * 100),
+            'vads_amount': int(float_round(values['amount'] * 100, prec)),
             'vads_currency': values.get('currency').number,
             'vads_trans_date': datetime.utcnow().strftime('%Y%m%d%H%M%S'),
             # SystemPay requires a unique 6-digits number between 000000 and 899999 per day
